af38918b34b7c4fb94f932c87f02b8b2460baefa
Krishan <75919169+krishan-aero@users.noreply.github.com>
2024-02-10 14:04:19 +0900

    Files added

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector("ul").lastElementChild.innerHTML = "Krishan Yadav";
+document.querySelector("li a").style.color = "red";
+document.querySelector("button").style.backgroundColor = "Yellow";
+document.querySelector("h1").classList.add("huge");
